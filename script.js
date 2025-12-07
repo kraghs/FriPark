a1f92b62dec82811692cd79bc49a7cfed7a48be6
kraghs <83667650+kraghs@users.noreply.github.com>
2025-12-07 21:46:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,10 @@ let userLng = 12.5683;
    Map init (kritisk: kør først)
    ========================= */
 const map = L.map('map', { preferCanvas: true }).setView([userLat, userLng], 6);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:'© OpenStreetMap contributors'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>',
+  subdomains: 'abcd',
+  maxZoom: 20
 }).addTo(map);
 
 /* =========================
